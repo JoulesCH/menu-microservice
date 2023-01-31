25d1865e3ca2391155a08de8ef317f49268ea89e
Isai <isai_alejandro@outlook.com>
2023-01-31 12:58:04 -0600

    :poop: ayura, porque no me nuestra platillos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 #  Se crea la instancia de FastAPI
@@ -7,7 +7,6 @@ app = FastAPI(
     description="Api for menu microservice",
     version="0.0.0",
 )
-
 
 #  A continuación se configura que todas las IPs puedan acceder a la API
 app.add_middleware(
@@ -18,11 +17,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+Platillos=[
+    {"Cafeteria_ID":9, "Platillo_ID":1, "Nombre":"Hamburguesa", "Precio":50},
+    {"Cafeteria_ID":9, "Platillo_ID":2, "Nombre":"Molletes", "Precio":30},
+    {"Cafeteria_ID":9, "Platillo_ID":3, "Nombre":"Chilaquiles", "Precio":60},
+    {"Cafeteria_ID":6, "Platillo_ID":1, "Nombre":"Hotdog", "Precio":25},
+    {"Cafeteria_ID":6, "Platillo_ID":2, "Nombre":"Papas", "Precio":30},
+    {"Cafeteria_ID":7, "Platillo_ID":1, "Nombre":"Agua", "Precio":10}
+]
 
 #  Routes
 @app.get("/")
 async def root():
-    return {"message": "olaolaola cuidado con la ola"}
+    return {"message": "jj"}
+
+#Muestra los platillos de determinada cafeteria
+def Platillo_Check(Cafeteria_ID):
+    if not Platillos[Cafeteria_ID]:
+        raise HTTPException(status_code=404, detail="Platillo Not Found")
+
+@app.get("/Menu/{Cafeteria_ID}")
+async def menu_list(Cafeteria_ID: int):
+    Platillo_Check(Cafeteria_ID)
+    return {"Platillo":Platillos[Cafeteria_ID]}
 
 #  Entry point
 if __name__ == "__main__":
@@ -36,3 +53,19 @@ if __name__ == "__main__":
         port=int(os.getenv("PORT", default=8080)), 
         reload=True
     )
+
+"""
+Diseño de base de datos de Menu:
+Cafeteria_ID
+Platillo_ID
+Nombre
+Precio
+Descripcion
+
+Diseño de base de datos de pedidos globales
+Cafeteria_ID
+Platillo_ID
+Nombre
+Precio
+Status
+"""
---
+++ prueba.py
@@ -0,0 +1,10 @@
+Platillos=[
+    {"Cafeteria_ID":9, "Platillo_ID":1, "Nombre":"Hamburguesa", "Precio":50},
+    {"Cafeteria_ID":9, "Platillo_ID":2, "Nombre":"Molletes", "Precio":30},
+    {"Cafeteria_ID":9, "Platillo_ID":3, "Nombre":"Chilaquiles", "Precio":60},
+    {"Cafeteria_ID":6, "Platillo_ID":1, "Nombre":"Hotdog", "Precio":25},
+    {"Cafeteria_ID":6, "Platillo_ID":2, "Nombre":"Papas", "Precio":30},
+    {"Cafeteria_ID":7, "Platillo_ID":1, "Nombre":"Agua", "Precio":10}
+]
+
+print(Platillos["Cafeteria_ID"==9])
